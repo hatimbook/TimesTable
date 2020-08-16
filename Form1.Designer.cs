f013a23f98329397a8e60a6d9caf0bc99ac61539
hatimbook <hatimbookwala@gmail.com>
2020-08-16 16:13:21 +0530

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,335 @@
+﻿namespace TimesTable
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.flowLayoutPanel2 = new System.Windows.Forms.FlowLayoutPanel();
+            this.trackBar2 = new System.Windows.Forms.TrackBar();
+            this.flowLayoutPanel3 = new System.Windows.Forms.FlowLayoutPanel();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.Colorbox = new System.Windows.Forms.CheckBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.flowLayoutPanel4 = new System.Windows.Forms.FlowLayoutPanel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.trackBar1 = new System.Windows.Forms.TrackBar();
+            this.label3 = new System.Windows.Forms.Label();
+            this.Factor = new System.Windows.Forms.NumericUpDown();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.hideAllControlsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.fullScreenToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.flowLayoutPanel1.SuspendLayout();
+            this.groupBox1.SuspendLayout();
+            this.flowLayoutPanel2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar2)).BeginInit();
+            this.flowLayoutPanel3.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.flowLayoutPanel4.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Factor)).BeginInit();
+            this.contextMenuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // flowLayoutPanel1
+            // 
+            this.flowLayoutPanel1.Controls.Add(this.groupBox1);
+            this.flowLayoutPanel1.Controls.Add(this.groupBox2);
+            this.flowLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Left;
+            this.flowLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.flowLayoutPanel1.Name = "flowLayoutPanel1";
+            this.flowLayoutPanel1.Padding = new System.Windows.Forms.Padding(3);
+            this.flowLayoutPanel1.Size = new System.Drawing.Size(140, 450);
+            this.flowLayoutPanel1.TabIndex = 0;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.flowLayoutPanel2);
+            this.groupBox1.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.groupBox1.Location = new System.Drawing.Point(3, 6);
+            this.groupBox1.Margin = new System.Windows.Forms.Padding(0, 3, 3, 3);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(134, 210);
+            this.groupBox1.TabIndex = 10;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Auto Play";
+            // 
+            // flowLayoutPanel2
+            // 
+            this.flowLayoutPanel2.Controls.Add(this.trackBar2);
+            this.flowLayoutPanel2.Controls.Add(this.flowLayoutPanel3);
+            this.flowLayoutPanel2.Controls.Add(this.button1);
+            this.flowLayoutPanel2.Controls.Add(this.progressBar1);
+            this.flowLayoutPanel2.Controls.Add(this.Colorbox);
+            this.flowLayoutPanel2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.flowLayoutPanel2.Location = new System.Drawing.Point(3, 16);
+            this.flowLayoutPanel2.Name = "flowLayoutPanel2";
+            this.flowLayoutPanel2.Size = new System.Drawing.Size(128, 191);
+            this.flowLayoutPanel2.TabIndex = 0;
+            // 
+            // trackBar2
+            // 
+            this.trackBar2.LargeChange = 1;
+            this.trackBar2.Location = new System.Drawing.Point(3, 3);
+            this.trackBar2.Maximum = 2;
+            this.trackBar2.Name = "trackBar2";
+            this.trackBar2.Orientation = System.Windows.Forms.Orientation.Vertical;
+            this.trackBar2.Size = new System.Drawing.Size(45, 93);
+            this.trackBar2.TabIndex = 0;
+            this.trackBar2.TickStyle = System.Windows.Forms.TickStyle.Both;
+            this.trackBar2.Scroll += new System.EventHandler(this.trackBar2_Scroll);
+            // 
+            // flowLayoutPanel3
+            // 
+            this.flowLayoutPanel3.Controls.Add(this.label4);
+            this.flowLayoutPanel3.Controls.Add(this.label5);
+            this.flowLayoutPanel3.Controls.Add(this.label6);
+            this.flowLayoutPanel3.Location = new System.Drawing.Point(54, 3);
+            this.flowLayoutPanel3.Name = "flowLayoutPanel3";
+            this.flowLayoutPanel3.Size = new System.Drawing.Size(63, 100);
+            this.flowLayoutPanel3.TabIndex = 1;
+            // 
+            // label4
+            // 
+            this.label4.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.label4.Location = new System.Drawing.Point(3, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(30, 25);
+            this.label4.TabIndex = 0;
+            this.label4.Text = "Slow";
+            this.label4.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // label5
+            // 
+            this.label5.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.label5.Location = new System.Drawing.Point(3, 25);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(44, 45);
+            this.label5.TabIndex = 1;
+            this.label5.Text = "Medium";
+            this.label5.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // label6
+            // 
+            this.label6.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.label6.Location = new System.Drawing.Point(3, 70);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(30, 20);
+            this.label6.TabIndex = 2;
+            this.label6.Text = "Fast";
+            this.label6.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.Green;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Location = new System.Drawing.Point(3, 109);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(114, 23);
+            this.button1.TabIndex = 8;
+            this.button1.Text = "Play";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(3, 138);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(114, 15);
+            this.progressBar1.Step = 1;
+            this.progressBar1.Style = System.Windows.Forms.ProgressBarStyle.Continuous;
+            this.progressBar1.TabIndex = 12;
+            // 
+            // Colorbox
+            // 
+            this.Colorbox.AutoSize = true;
+            this.Colorbox.Location = new System.Drawing.Point(3, 159);
+            this.Colorbox.Name = "Colorbox";
+            this.Colorbox.Size = new System.Drawing.Size(50, 17);
+            this.Colorbox.TabIndex = 13;
+            this.Colorbox.Text = "Color";
+            this.Colorbox.UseVisualStyleBackColor = true;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.flowLayoutPanel4);
+            this.groupBox2.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.groupBox2.Location = new System.Drawing.Point(3, 222);
+            this.groupBox2.Margin = new System.Windows.Forms.Padding(0, 3, 3, 3);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(134, 161);
+            this.groupBox2.TabIndex = 11;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Manual";
+            // 
+            // flowLayoutPanel4
+            // 
+            this.flowLayoutPanel4.Controls.Add(this.label1);
+            this.flowLayoutPanel4.Controls.Add(this.trackBar1);
+            this.flowLayoutPanel4.Controls.Add(this.label3);
+            this.flowLayoutPanel4.Controls.Add(this.Factor);
+            this.flowLayoutPanel4.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.flowLayoutPanel4.Location = new System.Drawing.Point(3, 16);
+            this.flowLayoutPanel4.Name = "flowLayoutPanel4";
+            this.flowLayoutPanel4.Size = new System.Drawing.Size(128, 142);
+            this.flowLayoutPanel4.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.flowLayoutPanel4.SetFlowBreak(this.label1, true);
+            this.label1.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.label1.Location = new System.Drawing.Point(0, 0);
+            this.label1.Margin = new System.Windows.Forms.Padding(0, 0, 3, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(134, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Point on Circle";
+            // 
+            // trackBar1
+            // 
+            this.flowLayoutPanel4.SetFlowBreak(this.trackBar1, true);
+            this.trackBar1.LargeChange = 2;
+            this.trackBar1.Location = new System.Drawing.Point(0, 16);
+            this.trackBar1.Margin = new System.Windows.Forms.Padding(0, 3, 3, 3);
+            this.trackBar1.Maximum = 400;
+            this.trackBar1.Minimum = 2;
+            this.trackBar1.Name = "trackBar1";
+            this.trackBar1.Size = new System.Drawing.Size(134, 45);
+            this.trackBar1.TabIndex = 9;
+            this.trackBar1.TickStyle = System.Windows.Forms.TickStyle.TopLeft;
+            this.trackBar1.Value = 2;
+            this.trackBar1.Scroll += new System.EventHandler(this.trackBar1_Scroll);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.flowLayoutPanel4.SetFlowBreak(this.label3, true);
+            this.label3.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.label3.Location = new System.Drawing.Point(3, 64);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(98, 13);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Multiplication Value";
+            // 
+            // Factor
+            // 
+            this.Factor.DecimalPlaces = 3;
+            this.flowLayoutPanel4.SetFlowBreak(this.Factor, true);
+            this.Factor.Increment = new decimal(new int[] {
+            1,
+            0,
+            0,
+            196608});
+            this.Factor.Location = new System.Drawing.Point(0, 80);
+            this.Factor.Margin = new System.Windows.Forms.Padding(0, 3, 3, 3);
+            this.Factor.Name = "Factor";
+            this.Factor.Size = new System.Drawing.Size(130, 20);
+            this.Factor.TabIndex = 7;
+            this.Factor.ValueChanged += new System.EventHandler(this.Factor_ValueChanged);
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.hideAllControlsToolStripMenuItem,
+            this.fullScreenToolStripMenuItem});
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(181, 70);
+            // 
+            // hideAllControlsToolStripMenuItem
+            // 
+            this.hideAllControlsToolStripMenuItem.Name = "hideAllControlsToolStripMenuItem";
+            this.hideAllControlsToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.hideAllControlsToolStripMenuItem.Text = "Hide All Controls";
+            this.hideAllControlsToolStripMenuItem.Click += new System.EventHandler(this.hideAllControlsToolStripMenuItem_Click);
+            // 
+            // fullScreenToolStripMenuItem
+            // 
+            this.fullScreenToolStripMenuItem.Name = "fullScreenToolStripMenuItem";
+            this.fullScreenToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.fullScreenToolStripMenuItem.Text = "Full Screen";
+            this.fullScreenToolStripMenuItem.Click += new System.EventHandler(this.fullScreenToolStripMenuItem_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ContextMenuStrip = this.contextMenuStrip1;
+            this.Controls.Add(this.flowLayoutPanel1);
+            this.DoubleBuffered = true;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "Form1";
+            this.Text = "Times Table Graphic";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.flowLayoutPanel1.ResumeLayout(false);
+            this.groupBox1.ResumeLayout(false);
+            this.flowLayoutPanel2.ResumeLayout(false);
+            this.flowLayoutPanel2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar2)).EndInit();
+            this.flowLayoutPanel3.ResumeLayout(false);
+            this.groupBox2.ResumeLayout(false);
+            this.flowLayoutPanel4.ResumeLayout(false);
+            this.flowLayoutPanel4.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Factor)).EndInit();
+            this.contextMenuStrip1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.NumericUpDown Factor;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TrackBar trackBar1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel2;
+        private System.Windows.Forms.TrackBar trackBar2;
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel4;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.CheckBox Colorbox;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem hideAllControlsToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem fullScreenToolStripMenuItem;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,241 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+
+namespace TimesTable
+{
+    public partial class Form1 : Form
+    {
+        #region GLOBALS
+        
+        private const int SLOWSPEED = 20;
+        private const int MEDSPEED = 10;
+        private const int HIGHSPEED = 5;
+        int numpoints = 200;
+        decimal factor = 2.0m;
+        bool autorunflag = false;
+        bool showcontrols = true;
+        bool fullscreen = false;
+        int autorunspeed = MEDSPEED;
+        bool endthread = false;
+        
+        #endregion
+
+        public Form1()
+        {
+            InitializeComponent();
+            trackBar1.Value = numpoints;
+            trackBar2.Value = 1;
+            Factor.Value = factor;
+            //Width = 1920;
+            //Height = 1080;
+            this.Paint += Form1_Paint;
+            this.FormClosing += Form1_FormClosing;
+            DoubleBuffered = true;
+
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            endthread = true;
+        }
+
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            Graphics g = e.Graphics;
+            g.Clear(Color.Black);
+            float RADIUS = (Height - 100) / 2;
+
+            //define paints and brushes
+            Pen mypen = new Pen(Color.White);
+            Brush mybrush = new SolidBrush(Color.Black);
+            Color mycolor1, mycolor2;
+            if(Colorbox.Checked)
+            {
+                mycolor1 = Color.FromArgb((byte)(factor * 2.5m), (byte)(250 - factor * 2.5m), 125);
+                mycolor2 = Color.FromArgb((byte)(250 - factor * 2.5m), (byte)(factor * 2.5m), 125);
+            }
+            else
+            {
+                mycolor1 = Color.GhostWhite;
+                mycolor2 = Color.GhostWhite;
+            }
+            Pen linepen = new Pen(mycolor1);
+            Pen linepen1 = new Pen(mycolor2);
+
+            // move origin to centre of canvas
+            g.TranslateTransform(Width / 2, Height / 2);
+            PointF origin = new PointF(0, 0);
+
+            // Draw the lines
+            decimal delta = (decimal)((2 * Math.PI / numpoints));
+            for (int i = 0; i < numpoints; i++)
+            {
+                double a = (double)((i % numpoints) * delta);
+                double b = (double)(((i * factor) % numpoints) * delta);
+                PointF pointa, pointb;
+                pointa = CirclePoint(RADIUS, a, origin);
+                pointb = CirclePoint(RADIUS, b, origin);
+                if (i % 2 == 0)
+                {
+                    g.DrawLine(linepen1, pointa, pointb);
+                }
+                else
+                {
+                    g.DrawLine(linepen, pointa, pointb);
+                }
+            }
+
+            // Draw the big circle
+            DrawCircle(RADIUS, origin, mypen, g);
+
+            if (!autorunflag)
+            {
+                button1.Text = "Play";
+                button1.BackColor = Color.ForestGreen;
+                progressBar1.Value = 0;
+                groupBox2.Visible = true;
+            }
+            else
+            {
+                button1.Text = "Stop";
+                button1.BackColor = Color.DarkRed;
+                progressBar1.Value = (int)factor;
+            }
+            
+            Factor.Value = factor;
+        }
+
+        #region PRIVATE_FUNCTIONS
+        // Draws a circle with a given pen of a given radius at the given origin point
+        private void DrawCircle(float radius, PointF origin, Pen p, Graphics g)
+        {
+            RectangleF rect = new RectangleF(origin.X - radius, origin.Y - radius, radius * 2, radius * 2);
+            g.DrawEllipse(p, rect);
+        }
+        
+        // determines point on a circle of given radius of given origin with a given angle
+        private PointF CirclePoint(float radius, double angleinradians, PointF origin)
+        {
+            float x = (float)(radius * Math.Cos(angleinradians) + origin.X);
+            float y = (float)(radius * Math.Sin(angleinradians) + origin.Y);
+            return new PointF(x, y);
+        }
+        #endregion
+
+        #region USERINTERFACE
+        private void trackBar1_Scroll(object sender, EventArgs e)
+        {
+            TrackBar val = (TrackBar)sender;
+            numpoints = (int)val.Value;
+            this.Invalidate();
+        }
+
+        private void trackBar2_Scroll(object sender, EventArgs e)
+        {
+            TrackBar speedbar = (TrackBar)sender;
+            switch (speedbar.Value)
+            {
+               
+                case 0:
+                    autorunspeed = HIGHSPEED;
+                    break;
+                default:
+                case 1:
+                    autorunspeed = MEDSPEED;
+                    break;
+
+                case 2:
+                    autorunspeed = SLOWSPEED;
+                    break;
+            };
+                    
+        }
+
+        private void Factor_ValueChanged(object sender, EventArgs e)
+        {
+            NumericUpDown val = (NumericUpDown)sender;
+            factor = (decimal)val.Value;
+            this.Invalidate();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Button thisbtn = (Button)sender;
+            factor = 1.0m;
+            Thread run = new Thread(autorun);
+            run.Start();
+            autorunflag = !autorunflag;
+            this.Invalidate();            
+        }
+
+        #endregion
+
+        private void autorun()
+        {
+            while (true)
+            {
+                if (autorunflag)
+                {
+                    while (decimal.Compare(factor, 100.00m) < 0)
+                    {
+                        factor = decimal.Add(factor, 0.001m);
+                        this.Invalidate();
+                        Thread.Sleep(autorunspeed);
+                        if(endthread | (!autorunflag))
+                        {
+                            this.Invalidate();
+                            break;
+                        }
+                    }
+                }
+                autorunflag = false;
+                break;
+            }
+        }
+
+        private void hideAllControlsToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            ToolStripMenuItem menu = (ToolStripMenuItem)sender;
+            if(showcontrols)
+            {
+                groupBox1.Visible = false;
+                groupBox2.Visible = false;
+                menu.Text = "Show Controls";
+            }
+            else
+            {
+                groupBox1.Visible = true;
+                groupBox2.Visible = true;
+                menu.Text = "Hide All Controls";
+            }
+            showcontrols = !showcontrols; 
+        }
+
+        private void fullScreenToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            ToolStripMenuItem menu = (ToolStripMenuItem)sender;
+            if (!fullscreen)
+            {
+                this.WindowState = FormWindowState.Normal;
+                this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+                this.Bounds = Screen.PrimaryScreen.Bounds;
+                menu.Text = "Exit Full Screen";
+            }
+            else
+            {
+                this.WindowState = FormWindowState.Maximized;
+                this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Sizable;
+                menu.Text = "Full Screen";
+            }
+            fullscreen = !fullscreen;
+        }
+    }
+}
